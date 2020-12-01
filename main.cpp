88062d94465f63b198a6e586556fd08b8afe41bd
michaelalr <70210070+michaelalr@users.noreply.github.com>
2020-12-01 18:37:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <windows.h>
 #include <iostream>
 #include <bcrypt.h>
-//#include <ntstatus.h>
-//#include <winnt.h>
-//#include <iomanip>
-
-//#pragma comment (lib, "bcrypt.lib")
 
 #define NT_SUCCESS(Status)          (((NTSTATUS)(Status)) >= 0)
 #define STATUS_UNSUCCESSFUL         ((NTSTATUS)0xC0000001L)
